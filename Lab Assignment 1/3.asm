1a8ee4f40b47f1e378f41802a1f6092c7707f2f6
Samidha Verma <samidha1705@gmail.com>
2017-09-09 23:19:49 +0530

    Create 3.asm

---
+++ Lab Assignment 1/3.asm
@@ -0,0 +1,20 @@
+; To find the largest of the given list of n data bytes stored in the memory starting from memory location 2071H.
+; Count of data bytes in the list (i.e. n) is stored at memory location 2070H.
+
+
+      LXI H, 2070H
+      MOV C, M
+      INX H
+
+      MOV A, M
+      INX H
+LOOP: CMP M
+      JNC SKIP
+      MOV A, M
+SKIP: INX H
+      DCR C
+      JNZ LOOP
+      
+      OUT 01H
+      
+      HLT
