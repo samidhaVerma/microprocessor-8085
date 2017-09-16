5e09e943f3d56838cea4f682c9e0a7b52e9e16d2
Samidha Verma <samidha1705@gmail.com>
2017-09-16 16:14:45 +0530

    Update 2.asm

--- Lab Assignment 1/2.asm
+++ Lab Assignment 1/2.asm
@@ -1,15 +1,20 @@
-; to find the largest of three numbers stored in the memory location 2050H, 2051H and 2052H, 
-; assuming that all three numbers are unequal.
+; To find the largest of three numbers stored in the memory location 2050H, 2051H and 2052H.
+; Assuming that all three numbers are unequal.
 
-       lxi h, 2050h
-       mov a, m
-       inx h
-       cmp m
-       jnc skip1
-       mov a,m
-skip1: inx h
-       cmp m
-       jnc skip2
-       mov a,m 
-skip2: out 01h
-       hlt
+       LXI H, 2050H
+       MOV A, M
+       INX H
+       CMP M
+       JNC SKIP1
+       
+       MOV A, M
+       
+SKIP1: INX H
+       CMP M
+       JNC SKIP2
+       
+       MOV A, M
+       
+SKIP2: OUT 01H
+       
+       HLT
