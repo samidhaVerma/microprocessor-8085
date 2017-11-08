ece034536ffa7b6322ac3ede3ebeae15ea0888a4
Samidha Verma <samidha1705@gmail.com>
2017-11-08 17:15:02 +0530

    Update main.asm

--- PSSP assignment/main.asm
+++ PSSP assignment/main.asm
@@ -1,13 +1,13 @@
 
 
 ;Data					
-;2555	Value of M1				
-;2556	Value of M2				
-;2557	Value of M3
+;2555H	Value of M1				
+;2556H	Value of M2				
+;2557H	Value of M3
         
-          MVI	A, 98	
+          MVI	A, 98H	
           OUT	03	
-	  MVI	A, 9A	
+	  MVI	A, 9AH	
 	  OUT	0B	
 START:    MOV   B, A	
           IN 	0A	    ;Input V
@@ -15,14 +15,14 @@ START:    MOV   B, A
 	  JZ	START	
 	  IN 	00	    ;Input Im cos phi
 	  CMA		
-	  SUI	80	
-	  STA	2552	
+	  SUI	80H	
+	  STA	2552H	
 	  IN 	00	      ;Input Vdc
 	  CMA		
-	  SUI	80	
-	  STA   2550	
+	  SUI	80H	
+	  STA   2550H	
 	  CALL  DIVISION	     ;M = 1 / Ycos phi
-	  LXI	H, 2553	    ;Stores M
+	  LXI	H, 2553H	    ;Stores M
           MOV	B, M	
 	  INX	H 	
 	  INX	H 	
@@ -40,28 +40,28 @@ START:    MOV   B, A
 	  JMP	START			
 					
 ;Delay subroutine					
-TRIP3:	  MVI	C, 06	      ;For 0.4 sec delay
+TRIP3:	  MVI	C, 06H	      ;For 0.4 sec delay
           JMP	THID	
-TRIP2:	  MVI	C, 03	      ;For 0.2 sec delay
-THID:	  MVI	D, FF	
-SEND:	  MVI	E, 33	
+TRIP2:	  MVI	C, 03H	      ;For 0.2 sec delay
+THID:	  MVI	D, FFH	
+SEND:	  MVI	E, 33H	
 FIRST:	  DCR	E 	
           JNZ	FIRST	
 	  DCR	D 	
 	  JNZ	SEND	
 	  DCR	C 	
 	  JNZ	THID	
-TRIP1:	  MVI	A, 01	
+TRIP1:	  MVI	A, 01H	
           OUT	01	
 	  JMP	START
 		
 
 ;Division subroutine
 DIVISION:					
-	   LHLD 2550	
-	   LDA	2552	
+	   LHLD 2550H	
+	   LDA	2552H	
 	   MOV	C, A	
-	   MVI	B, 08	
+	   MVI	B, 08H	
 DIV:	   DAD	H 	
     	   MOV	A, H	
 	   SUB	C 	
