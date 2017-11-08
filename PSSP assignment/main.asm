401fd3f54a17ce0d2221d4f356c74853c1c17b7a
Samidha Verma <samidha1705@gmail.com>
2017-11-08 17:11:02 +0530

    Update main.asm

--- PSSP assignment/main.asm
+++ PSSP assignment/main.asm
@@ -20,55 +20,55 @@ START:    MOV   B, A
 	  IN 	00	      ;Input Vdc
 	  CMA		
 	  SUI	80	
-	  STA 2550	
-	  CALL DIVISION	     ;M = 1 / Ycos phi
+	  STA   2550	
+	  CALL  DIVISION	     ;M = 1 / Ycos phi
 	  LXI	H, 2553	    ;Stores M
           MOV	B, M	
 	  INX	H 	
 	  INX	H 	
 	  MOV	A, M	     ;M1 in accumulator
 	  CMP	B 	
-	  JNC TRIP1	  ;Zone 1 trip
-	  INX   H 	
-      		MOV	A, M	     ;M2 in accumulator
-    	    CMP	B 	
-		      JNC TRIP2	   ;Zone 2 trip
-	       	INX H 	
-	       	MOV	A, M	      ;M3 in accumulator
-	       	CMP	B 	
-		      JNC TRIP3	    ;Zone 3 trip
-		      JMP	START			
+	  JNC   TRIP1	  ;Zone 1 trip
+	  INX   H 
+	  MOV	A, M	     ;M2 in accumulator
+	  CMP	B 	
+	  JNC   TRIP2	   ;Zone 2 trip
+	  INX   H 
+	  MOV	A, M	      ;M3 in accumulator
+	  CMP	B 	
+	  JNC   TRIP3	    ;Zone 3 trip
+	  JMP	START			
 					
 ;Delay subroutine					
 TRIP3:	  MVI	C, 06	      ;For 0.4 sec delay
-		      JMP	THID	
+          JMP	THID	
 TRIP2:	  MVI	C, 03	      ;For 0.2 sec delay
-THID:	    MVI	D, FF	
-SEND:	    MVI	E, 33	
+THID:	  MVI	D, FF	
+SEND:	  MVI	E, 33	
 FIRST:	  DCR	E 	
-		      JNZ	FIRST	
-		      DCR	D 	
-		      JNZ	SEND	
-		      DCR	C 	
-		      JNZ	THID	
+          JNZ	FIRST	
+	  DCR	D 	
+	  JNZ	SEND	
+	  DCR	C 	
+	  JNZ	THID	
 TRIP1:	  MVI	A, 01	
-		      OUT	01	
-		      JMP	START
+          OUT	01	
+	  JMP	START
 		
 
 ;Division subroutine
 DIVISION:					
-		      LHLD 2550	
-		      LDA	2552	
-		      MOV	C, A	
-		      MVI	B, 08	
-DIV:	    DAD	H 	
-    	    MOV	A, H	
-		      SUB	C 	
-		      JC	AHEAD	
-		      MOV	H, A	
-		      INR	L	
-AHEAD:	  DCR	B 	
-          JNZ	DIV	
-		      SHLD 2553H	;Quotient in 2553
-		      RET			 
+	   LHLD 2550	
+	   LDA	2552	
+	   MOV	C, A	
+	   MVI	B, 08	
+DIV:	   DAD	H 	
+    	   MOV	A, H	
+	   SUB	C 	
+	   JC	AHEAD	
+	   MOV	H, A	
+	   INR	L	
+AHEAD:	   DCR	B 	
+           JNZ	DIV	
+	   SHLD 2553H	;Quotient in 2553
+	   RET			 
